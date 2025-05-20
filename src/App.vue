379f49d585b70f225a820991a05260e6b7cd2ff4
Nay <nay.fullstack@gmail.com>
2025-05-20 14:10:46 +0200

    añado pagina home en app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,109 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue'
+
+const currentLang = ref('Hola')
+const showLogin = ref(false)
+
+const greetings = ['Hola', 'Hello', 'Kaixo', 'Bonjour', 'Ciao']
+let index = 0
+
+onMounted(() => {
+  setInterval(() => {
+    index = (index + 1) % greetings.length
+    currentLang.value = greetings[index]
+  }, 2500)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+
+    <div class="left">
+      <h1 class="greeting">{{ currentLang }}</h1>
+      <p class="login-text">
+        Accede al
+        <span class="login-link" @click="showLogin = true">Login</span>
+      </p>
+      <Login v-if="showLogin" />
+    </div>
+
+    <div class="right">
+      <div class="lines">
+        <div class="line orange-line"></div>
+        <div class="line purple-line"></div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.container {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  font-family: 'Segoe UI', sans-serif;
+  background-color: #ffffff;
+  color: #333;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.left {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.greeting {
+  font-size: 7rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+}
+
+.login-text {
+  font-size: 1.5rem;
+}
+
+.login-link {
+  color: #1e40af;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+.login-link:hover {
+  color: #0f172a;
+}
+
+.right {
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  gap: 0;
+  padding: 0;
+  margin: 0;
+}
+
+.line {
+  position: absolute;
+  margin-left: 20rem;
+  width: 120%;
+  top: 50%;
+  left: -10%;
+  opacity: 0.7;
+}
+
+.orange-line {
+  background-color: #ff4700;
+  transform: rotate(45deg);
+  height: 320px;
+}
+
+.purple-line {
+  background-color:#b268f7;
+  transform: rotate(-45deg);
+  height: 520px;
 }
 </style>
